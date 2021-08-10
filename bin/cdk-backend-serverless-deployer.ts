add8300d395eb3e3c3acd92d4c4fb2d88e5b32a0
jakemraz <jakemraz100@gmail.com>
2021-08-10 18:21:40 +0900

    init project

--- bin/cdk-backend-serverless-deployer.ts
+++ bin/cdk-backend-serverless-deployer.ts
@@ -1,21 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkBackendServerlessDeployerStack } from '../lib/cdk-backend-serverless-deployer-stack';
+import { CdkBackendServerlessDeployerStack } from '../stack/cdk-backend-serverless-deployer-stack';
+import { AppContext } from '../lib/app-context';
 
 const app = new cdk.App();
-new CdkBackendServerlessDeployerStack(app, 'CdkBackendServerlessDeployerStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const env = app.node.tryGetContext("env")==undefined?'dev':app.node.tryGetContext("env");
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+AppContext.getInstance().initialize({
+    applicationName: 'cbsd',
+    deployEnvironment: env
+})
+new CdkBackendServerlessDeployerStack(app, 'CdkBackendServerlessDeployerStack');
